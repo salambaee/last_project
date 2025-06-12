9e452d8fbdbb053a784577e980b89c09a73992db
Salam Rizq Mulia <salamrizki613@gmail.com>
2025-06-13 00:13:42 +0700

    Update gudang.dart

--- manajemen_gudang/lib/gudang.dart
+++ manajemen_gudang/lib/gudang.dart
@@ -1,9 +1,113 @@
 import 'barang.dart';
 import 'transaksi.dart';
 
+
 class Gudang {
   List<Barang> daftarBarang = [];
-  List<Transaksi> historiTransaksi = [];
+  final riwayatTransaksi = RiwayatTransaksi();
+
+  void tampilkanBarangUrutStokTerbesarKeTerkecil() {
+    List<Barang> barangTerurut = List.from(daftarBarang);
+    for (int i = 1; i < barangTerurut.length; i++) {
+      Barang key = barangTerurut[i];
+      int j = i - 1;
+
+      while (j >= 0 && barangTerurut[j].stokBarang < key.stokBarang) {
+        barangTerurut[j + 1] = barangTerurut[j];
+        j--;
+      }
+      barangTerurut[j + 1] = key;
+    }
+    print("+----+------------+----------------------+------------+");
+    print("| ID | Kode       | Nama Barang          | Stok       |");
+    print("+----+------------+----------------------+------------+");
+    for (Barang b in barangTerurut) {
+      final id = b.idBarang.toString().padLeft(2);
+      final kode = b.kodeBarang.padRight(10);
+      final nama = b.namaBarang.padRight(20);
+      final stok = b.stokBarang.toString().padLeft(10);
+      print("| $id | $kode | $nama | $stok |");
+    }
+    print("+----+------------+----------------------+------------+");
+  }
+
+  void tampilkanBarangUrutStokTerkecilKeTerbesar() {
+    List<Barang> barangTerurut = List.from(daftarBarang);
+    for (int i = 1; i < barangTerurut.length; i++) {
+      Barang key = barangTerurut[i];
+      int j = i - 1;
+
+      while (j >= 0 && barangTerurut[j].stokBarang > key.stokBarang) {
+        barangTerurut[j + 1] = barangTerurut[j];
+        j--;
+      }
+      barangTerurut[j + 1] = key;
+    }
+    print("+----+------------+----------------------+------------+");
+    print("| ID | Kode       | Nama Barang          | Stok       |");
+    print("+----+------------+----------------------+------------+");
+    for (Barang b in barangTerurut) {
+      final id = b.idBarang.toString().padLeft(2);
+      final kode = b.kodeBarang.padRight(10);
+      final nama = b.namaBarang.padRight(20);
+      final stok = b.stokBarang.toString().padLeft(10);
+      print("| $id | $kode | $nama | $stok |");
+    }
+    print("+----+------------+----------------------+------------+");
+  }
+
+  void tampilkanHistoriTransaksi() {
+    riwayatTransaksi.tampilkanRiwayatTransaksiTerbaru();
+  }
+
+  void barangKeluar(String kodeBarang, int jumlah) {
+    daftarBarang.sort((a, b) => a.kodeBarang.compareTo(b.kodeBarang));
+    int kiri = 0;
+    int kanan = daftarBarang.length - 1;
+    bool ditemukan = false;
+
+    while (kiri <= kanan) {
+      int tengah = (kiri + kanan) ~/ 2;
+      if (daftarBarang[tengah].kodeBarang == kodeBarang) {
+        daftarBarang[tengah].stokBarang -= jumlah;
+        riwayatTransaksi.tambahkanRiwayatTransaksi(Transaksi('Keluar', daftarBarang[tengah].namaBarang, jumlah));
+        print("Stok barang ${daftarBarang[tengah].namaBarang} berhasil keluar");
+        ditemukan = true;
+      } else if (kodeBarang.compareTo(daftarBarang[tengah].kodeBarang) < 0) {
+        kanan = tengah - 1;
+      } else {
+        kiri = tengah + 1;
+      }
+    }
+    if (!ditemukan) {
+      print("Barang dengan kode $kodeBarang tidak ditemukan");
+    }
+  }
+
+  void barangMasuk(String kodeBarang, int jumlah) {
+    daftarBarang.sort((a, b) => a.kodeBarang.compareTo(b.kodeBarang));
+    int kiri = 0;
+    int kanan = daftarBarang.length - 1;
+    bool ditemukan = false;
+
+    while (kiri <= kanan) {
+      int tengah = (kiri + kanan) ~/ 2;
+      if (daftarBarang[tengah].kodeBarang == kodeBarang) {
+        daftarBarang[tengah].stokBarang += jumlah;
+        riwayatTransaksi.tambahkanRiwayatTransaksi(Transaksi('Masuk', daftarBarang[tengah].namaBarang, jumlah));
+        print("Stok barang ${daftarBarang[tengah].namaBarang} berhasil ditambahkan.");
+        ditemukan = true;
+        break;
+      } else if (kodeBarang.compareTo(daftarBarang[tengah].kodeBarang) < 0) {
+        kanan = tengah - 1;
+      } else {
+        kiri = tengah + 1;
+      }
+    }
+    if (!ditemukan) {
+      print("Barang dengan kode $kodeBarang tidak ditemukan");
+    }
+  }
 
   void tambahBarang(Barang barang) {
     daftarBarang.add(barang);
@@ -39,7 +143,7 @@ class Gudang {
       print("| ID | Kode       | Nama Barang          | Stok       |");
       print("+----+------------+----------------------+------------+");
       for (var barang in daftarBarang) {
-        if (barang.namaBarang.toLowerCase() == namaBarangYangDicari.toLowerCase()) {
+        if (barang.namaBarang.toLowerCase().contains(namaBarangYangDicari.toLowerCase())) {
           final id = barang.idBarang.toString().padLeft(2);
           final kode = barang.kodeBarang.padRight(10);
           final nama = barang.namaBarang.padRight(20);
@@ -63,7 +167,7 @@ class Gudang {
     } 
     if (kodeBarangYangDicari != '') {
       for (var barang in daftarBarang) {
-        if (barang.kodeBarang.toLowerCase() == kodeBarangYangDicari.toLowerCase()) {
+        if (barang.kodeBarang.toLowerCase().contains(kodeBarangYangDicari.toLowerCase())) {
           print("+----+------------+----------------------+------------+");
           print("| ID | Kode       | Nama Barang          | Stok       |");
           print("+----+------------+----------------------+------------+");
@@ -84,7 +188,30 @@ class Gudang {
   }
 
   void cariBarangBerdasarkanId(int idBarangYangDicari) {
-
+    if (daftarBarang.isEmpty) {
+      print("Daftar barang kosong");
+      return;
+    }
+    if (idBarangYangDicari.toString() != '' || idBarangYangDicari >= 0) {
+      for (var barang in daftarBarang) {
+        if (barang.idBarang == idBarangYangDicari) {
+          print("+----+------------+----------------------+------------+");
+          print("| ID | Kode       | Nama Barang          | Stok       |");
+          print("+----+------------+----------------------+------------+");
+          final id = barang.idBarang.toString().padLeft(2);
+          final kode = barang.kodeBarang.padRight(10);
+          final nama = barang.namaBarang.padRight(20);
+          final stok = barang.stokBarang.toString().padLeft(10);
+          print("| $id | $kode | $nama | $stok |");
+          print("+----+------------+----------------------+------------+");
+        } else {
+          print("Barang tidak ditemukan!");
+          return;
+        }
+      }
+    } else {
+      print("Input tidak boleh kosong");
+    }
   }
 
   void cariBarangBerdasarkanStok(int stokBarangYangDicari) {
